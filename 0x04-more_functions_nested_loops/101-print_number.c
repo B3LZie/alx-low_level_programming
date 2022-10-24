d55ac2ba2edc24b7d53376b25514ccc6f6a44e63
B3LZie <belindapondayi@live.com>
2022-10-24 03:59:12 -0700

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,32 +7,40 @@
 
 void print_number(int n)
 {
-	if (n < 99)
+	int a, b, c, d, e;
+
+	if (n < 0)
 	{
-		_putchar(n / 10);
-		_putchar(n % 10);
+		_putchar('-');
+		n = n * -1;
+
 	}
-	else if (n < 999)
+	
+	if (n == 0)
 	{
-		_putchar(n / 100);
-
-		int a;
-
+		_putchar('0');
+	}
+	else if (n > 0 || n < 99)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n > 99 || n <= 999)
+	{
+		_putchar(n / 100 + '0');
 		a = n / 10;
-
-		_putchar(a % 10);
-		_putchar(n % 100);
+		d = a % 10;
+		e = n % 100;
+		_putchar(d + '0');
+		_putchar(e + '0');
 	}
 	else
 	{
-		_putchar(n / 1000);
-
-		int b, c;
-
+		_putchar(n / 1000 + '0');
 		b = n / 100;
 		c = n / 10;
-		_putchar(b % 10);
-		_putchar(c % 100);
-		_putchar(n % 1000);
+		_putchar(b % 10 + '0');
+		_putchar(c % 100 + '0');
+		_putchar(n % 1000 + '0');
 	}
 }
