cc43f137090476b79f6e41493b17a7ae82170b5c
B3LZie <belindapondayi@live.com>
2022-10-26 00:48:46 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of string str
+ * @str: string to be printed
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
