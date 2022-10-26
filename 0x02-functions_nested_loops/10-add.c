ad55cace451112e78efe3ce91d44d9f74c2e6d2d
B3LZie <belindapondayi@live.com>
2022-10-26 04:16:48 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adding two numbers and returning the result
+ * @int: var 1
+ * @int: var 2
+ *
+ * Return: return the sum of var 1 and var 2
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
