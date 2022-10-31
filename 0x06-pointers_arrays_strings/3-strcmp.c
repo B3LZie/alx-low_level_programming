abb6acc98b443981d8ec50eccc9b4b5ea7979299
B3LZie <belindapondayi@live.com>
2022-10-31 02:36:35 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,14 @@
  * _strcmp - COmpare strings
  * @s1: string 1
  * @s2: string 2
- * Return: the int 
+ * Return: the int
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i =0;
+	i = 0;
 	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
 		i++;
