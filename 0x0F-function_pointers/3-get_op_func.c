fe158131a6a8fb02a16c73dee57f41493e846cfa
B3LZie <belindapondayi@live.com>
2022-11-16 13:13:02 -0800

    Task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-cal.h"
+#include <string.h>
+
+/**
+ * get_op_func - getting the operation to carry out
+ * @s: the string
+ *
+ * Return: the operation
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_sub},
+		{"/", op_sub},
+		{"%", op_sub},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (strcmp((ops[i].op), s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+
+	return (0);
+}
