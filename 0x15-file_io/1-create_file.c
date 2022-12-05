6733edd67e15c3b3839035102a1d535310068637
B3LZie <belindapondayi@live.com>
2022-12-05 05:47:33 -0800

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to be created
+ * @text_content: the text to write
+ *
+ * Return: 1 success -1 failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, w;
+
+	if (filename == NULL)
+		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return(-1);
+	if (i)
+	{
+		w = write(fd, text_content, i);
+	}
+	close(fd);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
