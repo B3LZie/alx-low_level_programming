ecad009d4f1a631bcc658aefa556140396e2a07b
B3LZie <belindapondayi@live.com>
2022-12-06 01:20:44 -0800

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	w = write(fd, text_content, i);
