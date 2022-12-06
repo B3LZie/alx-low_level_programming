66235f1f517f6c721729ef8203e9503dae3ab960
B3LZie <belindapondayi@live.com>
2022-12-06 01:04:36 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return(-1);
+		return (-1);
 	if (i)
 	{
 		w = write(fd, text_content, i);
